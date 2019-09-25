b79f10bca2699d4e0e92633c9213f352e28637ae
Dongjoon Hyun <dhyun@apple.com>
2019-09-25 09:50:12 +0000

    JDK8

--- a.sh
+++ a.sh
@@ -1,11 +1,12 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
-declare -A modules=( ["core"]="core" ["sql/catalyst"]="catalyst" ["sql/core"]="sql" ["sql/hive"]="hive" ["external/avro"]="avro" ["mllib"]="mllib" )
+#declare -A modules=( ["core"]="core" ["sql/catalyst"]="catalyst" ["sql/core"]="sql" ["sql/hive"]="hive" ["external/avro"]="avro" ["mllib"]="mllib" )
+declare -A modules=( ["core"]="core" ["sql/catalyst"]="catalyst" ["sql/hive"]="hive" ["external/avro"]="avro" ["mllib"]="mllib" )
 for MODULE in "${!modules[@]}"
 do
     for j in $(for i in $(find "$MODULE" -name "*Benchmark-results.txt" | sort); do find . -name $(basename $i | sed 's/-results.txt//').scala; done)
     do
         NAME=$(echo $j | sed 's/^.*scala\///g' | sed 's/\//./g' | sed 's/.scala//g')
-        echo "SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt \"${modules[$MODULE]}/test:runMain $NAME\""
+        SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt "${modules[$MODULE]}/test:runMain $NAME"
     done
 done
