e6f9ae4504d2dc1c71d7c63a4e3c44e2e9f10e98
Dongjoon Hyun <dhyun@apple.com>
2019-09-24 23:41:34 -0700

    Fix description

---
+++ a.sh
@@ -0,0 +1,11 @@
+#!/usr/local/bin/bash
+
+declare -A modules=( ["core"]="core" ["sql/catalyst"]="catalyst" ["sql/core"]="core" ["sql/hive"]="hive" ["external/avro"]="avro" ["mllib"]="mllib" )
+for MODULE in "${!modules[@]}"
+do
+    for j in $(for i in $(find "$MODULE" -name "*Benchmark-results.txt" | sort); do find . -name $(basename $i | sed 's/-results.txt//' | sed 's/-jdk11//').scala; done)
+    do
+        NAME=$(echo $j | sed 's/^.*scala\///g' | sed 's/\//./g' | sed 's/.scala//g')
+        echo "SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt \"${modules[$MODULE]}/test:runMain $NAME\""
+    done
+done
