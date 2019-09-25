e33f729bdd4b63401dd2fc11ea2785b5d2a4f75d
Dongjoon Hyun <dhyun@apple.com>
2019-09-25 21:46:31 +0000

    updat

--- a.sh
+++ a.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 #declare -A modules=( ["core"]="core" ["sql/catalyst"]="catalyst" ["sql/core"]="sql" ["sql/hive"]="hive" ["external/avro"]="avro" ["mllib"]="mllib" )
-declare -A modules=( ["core"]="core" ["sql/catalyst"]="catalyst" ["sql/hive"]="hive" ["external/avro"]="avro" ["mllib"]="mllib" )
+declare -A modules=( ["sql/core"]="sql" )
 for MODULE in "${!modules[@]}"
 do
     for j in $(for i in $(find "$MODULE" -name "*Benchmark-results.txt" | sort); do find . -name $(basename $i | sed 's/-results.txt//').scala; done)
     do
         NAME=$(echo $j | sed 's/^.*scala\///g' | sed 's/\//./g' | sed 's/.scala//g')
-        SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt "${modules[$MODULE]}/test:runMain $NAME"
+        if [ $NAME == "org.apache.spark.sql.execution.ExternalAppendOnlyUnsafeRowArrayBenchmark" ]; then
+            echo SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt ";project sql;set javaOptions in Test += \"-Dspark.memory.debugFill=false\";${modules[$MODULE]}/test:runMain $NAME"
+        else
+            echo SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt "${modules[$MODULE]}/test:runMain $NAME"
+        fi
     done
 done
