591b2ed6a23f9027b14888b8662fb1055f49ddcf
Dongjoon Hyun <dhyun@apple.com>
2019-10-23 18:54:11 -0700

    a

--- dev/benchmark.sh
+++ dev/benchmark.sh
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Run benchmark scripts for both JDK8/11.
+# A utility script to run benchmarks for both JDK8/11.
 # Usage:
 #
 #     Run a single benchmark:
